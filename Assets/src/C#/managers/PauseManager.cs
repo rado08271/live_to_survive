c139b54474c01bfd82a7d4fc0a0acba80469b6b8
Radoslav Figura <radoslav.figura@hotovo.org>
2020-04-10 23:21:51 +0100

    COV-052-Pause Menu Added new pause menu into game with abillity to resume or go to main menu

---
+++ Assets/src/C#/managers/PauseManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using eu.parada.game;
+
+namespace eu.parada.manager {
+    public class PauseManager : MonoBehaviour {
+
+        public Game game;
+        public void pauseGame() {
+            Debug.Log("Game Paused");
+        }
+
+        public void quitGame() {
+            game.exit();
+            SceneManager.LoadScene(0);
+        }
+
+        // Update is called once per frame
+        void Update() {
+            
+        }
+    }
+}
