25e075d5a20c97ea27c4b3f86dcef0725ba13028
Radoslav Figura <radoslav.figura@hotovo.org>
2020-04-27 22:08:43 +0100

    COV-068 Saved project

---
+++ Assets/src/C#/PostGame.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using eu.parada.game;
+using eu.parada.manager;
+using eu.parada.enums;
+using eu.parada.common;
+
+namespace eu.parada {
+    public class PostGame : MonoBehaviour {
+
+        public Image background;
+        public GameObject loading;
+
+        public Text gameResult;
+
+        public Text gameResultInfo;
+
+        public Text userName;
+        public Text userScore;
+        public Text days;
+        public Text effectsAvailable;
+        public Text money;
+
+        private bool loaded = false;
+        private bool init = false;
+        private GamePlay game = Manager.getInstance().getGamePlay();
+
+        void Setup() {
+        }
+
+        // Update is called once per frame
+        void Update() {
+            if ( !init ) {
+                StartCoroutine(initGameAfter(1));
+                init = true;
+            }
+
+            if (loaded) {
+                if ( game.gameState == GameState.FAILED) {
+                    gameResult.text = game.user.name + "! " + StringConstant.LOST_GAME_FAILED_DEAD;
+                    background.color = new Color(0.21f, 0.027f, 0);
+                } else if (game.gameState == GameState.NOTIME) {
+                    gameResult.text = game.user.name + "! " + StringConstant.NO_TIME_BASIC;
+                    background.color = new Color(0f, 0, 0);
+                } else if (game.gameState == GameState.WON) {
+                    gameResult.text = game.user.name + "! " + StringConstant.WON_GAME_STATE;
+                    background.color = new Color(0f, 0.047f, 0.21f);
+                } else {
+                    gameResult.text = StringConstant.UNKNOWN_GAMESTATE_ERROR;
+                    background.color = new Color(0f, 0f, 0f);
+
+                }
+
+                userName.text = "Player " + game.user.name;
+                userScore.text = "Your final score is " + game.user.score.ToString();
+                days.text = "It took you " + game.getCurrentDay().ToString() + " of " + game.getMaxDays() + " days";
+                money.text = "Final DNA " + ((int) game.lungs.vitals.money.currentMoney).ToString();
+                effectsAvailable.text = "You gained " + game.lungs.boughtEffects.Count.ToString() + " knowladge out of 5";
+            }
+        }
+
+        public void quitGame() {
+            // UNLOADING PROCESS
+            loaded = false;
+
+            Manager.getInstance().stopGame();
+            SceneManager.UnloadScene(SceneManager.GetActiveScene());
+            Resources.UnloadUnusedAssets();
+            Destroy(this);
+
+            SceneManager.LoadScene(0, LoadSceneMode.Single);
+
+            Manager.getInstance().stopGame();
+        }
+
+        private IEnumerator initGameAfter(int seconds) {
+            loading.SetActive(true);
+            yield return new WaitForSeconds(seconds);
+            loading.SetActive(false);
+            game = Manager.getInstance().getGamePlay();
+            if ( game != null) loaded = true;      
+        }
+    }
+}
---
+++ Assets/src/C#/common/StringMessage.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace eu.parada.common {
+    public class StringMessage {
+
+        private bool wasShown = false;
+        private bool clickable = false;
+        private bool titled = false;
+        private int length = 0;
+        private string title = "";
+        private string content = "";
+        private bool loggable = false;
+
+        public StringMessage(string content, bool loggable) {
+            this.wasShown = true;
+            this.loggable = loggable;
+            this.content = content;
+        }
+
+        public StringMessage(string content) {
+            this.content = content;
+            this.clickable = true;
+        }
+
+        public StringMessage(string content, int length) {
+            this.content = content;
+            this.length = length;
+        }
+
+        public StringMessage(string content, string title) {
+            this.content = content;
+            this.title = title;
+            this.clickable = true;
+            this.titled = true;
+        }
+
+        public StringMessage(string content, string title, int length) {
+            this.content = content;
+            this.title = title;
+            this.length = length;
+            this.titled = true;
+        }
+
+        public void setLoggable(bool loggable) {
+            this.loggable = loggable;
+        }
+        public void show() {
+            this.wasShown = true;
+        }
+
+        public bool messageWasShown() {
+            return this.wasShown;
+        }
+
+        public bool isLoggable() {
+            return this.loggable;
+        }
+
+        public bool isTitled() {
+            return this.titled;
+        }
+
+        public bool isClickable() {
+            return this.clickable;
+        }
+
+        public string getTitle() {
+            return this.title;
+        }
+
+        public string getContent() {
+            return this.content;
+        }
+
+        public int getLength() {
+            return this.length;
+        }
+
+    }
+}
